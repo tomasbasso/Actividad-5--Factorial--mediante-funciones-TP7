1c49532c3d0a12dd921523f09f1ab1cfc705818b
Tomás Basso Fernández <tomas.basso@hotmail.com>
2022-10-06 18:21:31 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Actividad_5__Factorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            int n;
+
+
+            Console.WriteLine("Ingrese el numero a factorizar");
+            n = Convert.ToInt32(Console.ReadLine());
+            formula(n);
+            Console.WriteLine("El factorial de {0} es {1}", n, formula(n));
+            
+        }
+        static int formula(int n)
+        {
+            int i,f;
+            f = 1;
+            for (i = 1; i <= n; i++)
+            {
+                f = f * i;
+            }
+            return f;
+        }
+    }
+}
